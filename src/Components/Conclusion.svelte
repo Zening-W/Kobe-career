6e7b4770ef146168e293cc8cb002e8c98919991b
Kening Li <103562029+Rika-Li@users.noreply.github.com>
2024-06-05 22:11:12 -0700

    Update Conclusion.svelte

--- src/Components/Conclusion.svelte
+++ src/Components/Conclusion.svelte
@@ -39,7 +39,7 @@
     var conclusionSection = document.getElementById('conclusion');
     
     var image = document.createElement('img');
-    image.src = "public/assets/img/end.jpg"; // Replace with the actual image source
+    image.src = "/Kobe-career/public/assets/img/end.jpg";
     image.alt = 'Kobe Bryant';
     image.style.width = '80%';
     image.style.maxWidth = '100%';
