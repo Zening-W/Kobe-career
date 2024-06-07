d21e08ebc5f124dac5b40669d3ca4acbd68d5671
Kening Li <103562029+Rika-Li@users.noreply.github.com>
2024-06-06 19:49:57 -0700

    Update Conclusion.svelte

--- src/Components/Conclusion.svelte
+++ src/Components/Conclusion.svelte
@@ -3,9 +3,13 @@
   <p class="body-text" id="conclusion-text">
     Known for his scoring prowess, relentless work ethic, and clutch performances, Kobe won five NBA championships, earned two Finals MVP awards, and was an 18-time All-Star. His career totals include over 33,000 points, ranking him among the top scorers in NBA history. Kobe's influence extended beyond the court, inspiring countless athletes worldwide.
   </p>
+  <img src={end} />
 </section>
 
 <script>
+
+  import end from '/assets/img/end.jpg';
+
   // Function to show text with blinking effect once
   function showText(id, delay) {
     setTimeout(() => {
@@ -38,22 +42,22 @@
   document.addEventListener("DOMContentLoaded", () => {
     var conclusionSection = document.getElementById('conclusion');
     
-    var image = document.createElement('img');
-    image.src = "/public/assets/img/end.jpg";
-    image.alt = 'Kobe Bryant';
-    image.style.width = '80%';
-    image.style.maxWidth = '100%';
-    image.style.height = 'auto';
-    image.style.border = '2px solid #333';
-    image.style.borderRadius = '4px';
-    image.style.opacity = '0'; // Initial hidden state
-    image.style.transition = 'opacity 1s ease-in-out';
-    image.style.margin = '1rem auto';
-    image.style.display = 'block'; // Center the image horizontally
-    image.id = 'conclusion-image';
+    // var image = document.createElement('img');
+    // image.src = "/img/end.jpg";
+    // image.alt = 'Kobe Bryant';
+    // image.style.width = '80%';
+    // image.style.maxWidth = '100%';
+    // image.style.height = 'auto';
+    // image.style.border = '2px solid #333';
+    // image.style.borderRadius = '4px';
+    // image.style.opacity = '0'; // Initial hidden state
+    // image.style.transition = 'opacity 1s ease-in-out';
+    // image.style.margin = '1rem auto';
+    // image.style.display = 'block'; // Center the image horizontally
+    // image.id = 'conclusion-image';
 
-    // Append the image after the conclusion section
-    conclusionSection.insertAdjacentElement('afterend', image);
+    // // Append the image after the conclusion section
+    // conclusionSection.insertAdjacentElement('afterend', image);
     
     // Show the text and then the image with delays
     showText("conclusion-text", 1000); // Show text after 1 second
